b1c3b8a411aa58a3d16e4c0d87310be4fca5bfaf
Luis Choque <luis.choque.castro@outlook.com>
2023-09-21 18:30:39 -0500

    Fixed cookie thing in incognito

--- src/plugins/logger.plugin.ts
+++ src/plugins/logger.plugin.ts
@@ -28,8 +28,12 @@ const emojiStatus = new Map([
 const parseCookie = (str: string) => str.split(';')
     .map((v: string) => v.split('='))
     .reduce((acc: any, v: string[]) => {
-    acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
-    return acc;
+        try {
+            acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+            return acc;
+        } catch (error) {
+            return acc;
+        }
     }, {});
 
 export const loggerPlugin = new Elysia()
